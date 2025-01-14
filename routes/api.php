c472ef56312942f242e35eb937a539f7606c893c
vegi.daniel <vegi.dani1@gmail.com>
2025-01-14 15:37:11 +0100

    api route fixed

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,8 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-
-    Route::get('/userget', [UserController::class, 'index']);
-    Route::post('/userstore', [UserController::class, 'store']);
-    Route::destroy('/userdestroy/{id}', [UserController::class, 'destroy']);
-    Route::get('/usergetone/{id}', [UserController::class, 'getone']);
-});
+Route::get('/users', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{id}', [UserController::class, 'getone']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
