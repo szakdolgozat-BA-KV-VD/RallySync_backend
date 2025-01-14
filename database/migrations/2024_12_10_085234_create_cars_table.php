92521862bd2728323ae5712c0f9dd5128fbff854
vegi.daniel <vegi.dani1@gmail.com>
2025-01-14 15:04:24 +0100

    breeze added

--- database/migrations/2024_12_10_085234_create_cars_table.php
+++ database/migrations/2024_12_10_085234_create_cars_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Car;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,8 @@
 <?php
 
-use App\Http\Controllers\CarController;
-use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/cars', [CarController::class, 'index']);
-Route::get('/cars2', [CarController::class, 'osszesAutoMarkajaCategoriakkalAllapottal']);
-Route::get('/foglaltAutok', [CarController::class, 'osszesFoglaltAuto']);
-Route::get('/szabadAutok', [CarController::class, 'osszesSzabadAuto']);
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
